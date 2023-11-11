8a27e15f480ad0cea8a8850eea116ae2c4cdaa28
deepikavelusamy <deepikavelusamy@gmail.com>
2023-11-11 10:03:43 -0500

    cookies and redirecting

--- api/index.js
+++ api/index.js
@@ -3,9 +3,14 @@ const req = require('express/lib/request');
 const res = require('express/lib/response');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 require('dotenv').config()
 const app = express();
+
+const bcrypySalt = bcrypt.genSaltSync(12);
+const jwtSecret = 'guwgOLihdjkeDFhiGFGuhfiuoqKJdjhsb';
 
 app.use(express.json());
 app.use(cors({
@@ -13,7 +18,7 @@ app.use(cors({
     origin:'http://localhost:5173',
 }));
 
-console.log(process.env.MONGO_URL)
+
 mongoose.connect(process.env.MONGO_URL);
 
 
@@ -21,14 +26,40 @@ app.get('/test',(req,res) => {
     res.json('test ok');
 });
 
-app.post('/register',(req,res) => {
+app.post('/register', async (req,res) => {
     const {name,email,password} = req.body;
-    User.create({
-        name,
-        email,
-        password,
-    });
-    res.json({name,email,password});
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password:bcrypt.hashSync(password, bcrypySalt),
+        });
+        res.json({userDoc});
+
+    } catch(e){
+        res.status(422).json(e);
+    }
+    
+    
 })
+
+app.post('/login', async (req,res) => {
+   const {email,password} = req.body;
+   const userDoc = await User.findOne({email});
+   if(userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password)
+    if(passOk){
+        jwt.sign({email:userDoc.email, id:userDoc._id},jwtSecret,{},(err,token) => {
+            if(err) throw (err);
+            res.cookie('token', token ).json('pass Ok'); 
+        } );
+        
+   } else{
+    res.status(422).json('pass not Ok');
+   }
+} else {
+    res.json('not found');
+   }
+});
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,20 @@ export default function RegisterPage(){
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
-    function registerUser(ev){
+    async function registerUser(ev){
         ev.preventDefault();
-        axios.post('/register',{
-            name,
-            email,
-            password,
-        });
+        try{
+            await axios.post('/register',{
+                name,
+                email,
+                password,
+            });
+            alert('Registration Successful. Now you can login');
+
+        } catch(e){
+            alert('Registration Failed. Please try again later');
+        }
+        
     }
     return (
         <div className="mt-4 grow flex items-center justify-around">
